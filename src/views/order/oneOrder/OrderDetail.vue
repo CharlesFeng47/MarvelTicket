88327c51c575aa46223dd9a1f9ff8390d32137ab
CharlesFeng <fdfengjunjie@126.com>
2018-03-18 00:17:09 +0800

    UPDATE：订单展示

--- src/views/order/oneOrder/OrderDetail.vue
+++ src/views/order/oneOrder/OrderDetail.vue
@@ -1,6 +1,78 @@
 <template>
     <div>
-      <h1>单个订单详情</h1>
+      <h1>订单编号：1234565432</h1>
+
+      <!--基本信息-->
+      <el-row class="basic_info">
+        <el-col :span="8">
+          <el-col :span="6">
+            <h3>举行场馆</h3>
+          </el-col>
+          <el-col :span="18">
+            <el-tag type="primary" class="basic_content">江苏大剧院</el-tag>
+          </el-col>
+        </el-col>
+        <el-col :span="8">
+          <el-col :span="6">
+            <h3>计划名称</h3>
+          </el-col>
+          <el-col :span="18">
+            <el-tag type="primary" class="basic_content">2018大寺大街上的撒娇的撒娇</el-tag>
+          </el-col>
+        </el-col>
+        <el-col :span="8">
+          <el-col :span="10">
+            <h3>计划预计开始时间</h3>
+          </el-col>
+          <el-col :span="14">
+            <el-tag type="danger" class="basic_content">2018-21-21</el-tag>
+          </el-col>
+        </el-col>
+        <el-col :span="8">
+          <el-col :span="6">
+            <h3>订票类型</h3>
+          </el-col>
+          <el-col :span="18">
+            <el-tag type="primary" class="basic_content">立即购票</el-tag>
+          </el-col>
+        </el-col>
+        <el-col :span="8">
+          <el-col :span="6">
+            <h3>订票状态</h3>
+          </el-col>
+          <el-col :span="18">
+            <el-tag type="primary" class="basic_content">已购票</el-tag>
+          </el-col>
+        </el-col>
+        <el-col :span="8">
+          <el-col :span="6">
+            <h3>订票总价</h3>
+          </el-col>
+          <el-col :span="18">
+            <el-tag type="warning" class="basic_content">3280</el-tag>
+          </el-col>
+        </el-col>
+      </el-row>
+
+      <!--座位价格映射-->
+      <el-row class="basic_info">
+        <el-col :span="3">
+          <h3>订单座位详情</h3>
+        </el-col>
+      </el-row>
+      <el-row class="basic_info" type="flex" justify="space-around">
+        <el-col :offset="2">
+          <el-table :data="seatPriceNumMap" style="width: 500px" stripe show-summary>
+            <el-table-column
+              v-for="{ prop, label } in colConfigs"
+              align="center"
+              :key="prop"
+              :prop="prop"
+              :label="label">
+            </el-table-column>
+          </el-table>
+        </el-col>
+      </el-row>
     </div>
 </template>
 
@@ -18,6 +90,24 @@
         'choose_seats',
         'choose_seats_count'
       ])
+    },
+    data() {
+      this.colConfigs = [
+        { prop: 'seatName', label: '坐席名称' },
+        { prop: 'num', label: '订购数量' },
+        { prop: 'price', label: '计价' }
+      ]
+
+      return {
+        seatPriceNumMap: [
+          { seatName: 'das', num: 100, price: 200 },
+          { seatName: 'das', num: 100, price: 200 },
+          { seatName: 'das', num: 100, price: 200 },
+          { seatName: 'das', num: 100, price: 200 },
+          { seatName: 'das', num: 100, price: 200 },
+          { seatName: 'das', num: 100, price: 200 }
+        ]
+      }
     },
     mounted: function() {
       if (this.isNew === 'false') {
@@ -40,5 +130,11 @@
 </script>
 
 <style scoped>
+  .basic_info {
+    margin: 5px;
+  }
 
+  .basic_content {
+    margin-top: 12px;
+  }
 </style>
